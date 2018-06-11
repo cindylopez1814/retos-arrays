ec04bae40fb65ba193963e26ecce4f4b66e342f1
Cindy Lopez <cindy.lopezoportus@gmail.com>
2018-06-11 11:20:03 -0400

    7 retos

--- app.js
+++ app.js
@@ -1,42 +1,41 @@
-function computeAverageLengthOfWords(word1, word2) {
-  // your code here
-  word1 = word1.length;
-  word2 = word2.length;
-  
-  let promedio = (word1 + word2)/2;
-  return promedio;
-}
-
+const computeAverageLengthOfWords = (word1, word2) => (word1.length + word2.length)/2;
 // 
-function convertDoubleSpaceToSingle(str) {
-  // your code here
-  str = str.split("  ");
-  return str.join(" "); 
-}
-
+const convertDoubleSpaceToSingle = (str) => str = str.split("  ").join(" ");
 //
-function areValidCredentials(name, password) {
-  // your code here
+const areValidCredentials = (name, password) => {
   if (name.length > 3 && password.length > 8){
     return true;
   }
   else return false;
 }
-
 //
-function filterOddElements(arr) {
-  // your code here
+const filterOddElements = (arr) => {
   const evenNumbers = arr.filter(element => element % 2 !== 0);
   return evenNumbers;
 }
-
 //
-function computeSumOfAllElements(arr) {
-  // your code here
+const computeSumOfAllElements = (arr) => {
     const newNumbers = arr.reduce((previousValue, currentValue) => {
     return previousValue + currentValue;
   });
   return newNumbers;
 }
 //
+const getNthElement = (array, n) => {
+  if (array === 0){
+    return undifined;
+  } else {
+    return array[n];
+  }
+}
+//
+const findPairForSum = (array, number) => {
+  for (let i = 0; i < array.length; i++) {
+    for(let index = 0; index < array.length; index++) {
+      if (array[i] + array[index] === number){
+      return [array[i],array[index]];
+      }
+    }
+  }
+}
 
